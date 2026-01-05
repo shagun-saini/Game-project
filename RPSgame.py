37e9408f99b4655e96a77df868ebd996c74b017e
shagun-saini <shagunsaini205@gmail.com>
2026-01-06 00:47:15 +0530

    Add a limit line

---
+++ RPSgame.py
@@ -0,0 +1,23 @@
+import random
+
+choice=["Rock","Paper","Scissor"]
+computer=random.choice(choice)
+
+print("Rock\n Paper\n Scissor")
+
+user_choice=input("Choose the Rock,Paper Scissor")
+
+print("User choice:",user_choice)
+print("computer choice",computer)
+attempt=5
+if(user_choice==computer):
+    print("Draw")
+elif(user_choice=="Rock" and computer=="Paper")or\
+    (user_choice=="Scissor" and computer=="Paper")or\
+    (user_choice=="Rock" and computer=="Scissor"):
+    print("user wins")
+else:
+    print("computer wins")    
+    attempt-=1
+    print("Attempt left:",attempt)
+    
