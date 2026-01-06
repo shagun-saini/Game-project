338a3656c8756d11b77647a0b2349b93b29b28bf
shagun-saini <shagunsaini205@gmail.com>
2026-01-06 20:20:42 +0530

    Fixed logic and indentation

--- RPSgame.py
+++ RPSgame.py
@@ -1,3 +1,4 @@
+# Rock Paper Scissor game
 import random
 
 choice=["Rock","Paper","Scissor"]
@@ -5,21 +6,24 @@ computer=random.choice(choice)
 
 print("Rock\n Paper\n Scissor")
 
-user_choice=input("Choose the Rock,Paper Scissor")
-
-print("User choice:",user_choice)
-print("computer choice",computer)
 attempt=5  
-while attempt>0:
+
+for i in range(attempt):
+ print(f"Attempt:\n{i+1}")
+
+ user_choice=input("Choose the Rock,Paper Scissor")
+
+ print("User choice:",user_choice)
+ print("computer choice",computer)
+
+
  if(user_choice==computer):
     print("Draw")
  elif(user_choice=="Rock" and computer=="Paper")or\
     (user_choice=="Scissor" and computer=="Paper")or\
     (user_choice=="Rock" and computer=="Scissor"):
     print("user wins")
- else:
-    print("computer wins") 
-      
-    attempt-=1
-    print("Attempt left:",attempt)
     
+ else:
+    print("computer wins")    
+   
