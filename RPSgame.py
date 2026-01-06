b14d7f2c7c124ffbaf363c213746af872c828fdf
shagun-saini <shagunsaini205@gmail.com>
2026-01-06 18:42:31 +0530

    Updated loop

--- RPSgame.py
+++ RPSgame.py
@@ -9,15 +9,17 @@ user_choice=input("Choose the Rock,Paper Scissor")
 
 print("User choice:",user_choice)
 print("computer choice",computer)
-attempt=5
-if(user_choice==computer):
+attempt=5  
+while attempt>0:
+ if(user_choice==computer):
     print("Draw")
-elif(user_choice=="Rock" and computer=="Paper")or\
+ elif(user_choice=="Rock" and computer=="Paper")or\
     (user_choice=="Scissor" and computer=="Paper")or\
     (user_choice=="Rock" and computer=="Scissor"):
     print("user wins")
-else:
-    print("computer wins")    
+ else:
+    print("computer wins") 
+      
     attempt-=1
     print("Attempt left:",attempt)
     
