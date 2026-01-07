394387c078daa840f02b1f6efa5385b575125b7b
shagun-saini <shagunsaini205@gmail.com>
2026-01-07 22:18:58 +0530

    remove the line

--- Mastermind game.py
+++ Mastermind game.py
@@ -5,31 +5,44 @@ import random
 play1=random.randrange(100,500)
 play2=int(input("Enter the guessing num"))
 print("Player1 guessing number",play1) #print the guessing number
-print("Player1 guessing number",play2) #print the guessing number
+print("Player2 guessing number",play2) #print the guessing number
 
 if(play1==play2): #Check the condition
     print("Great! You guess the corect number")
 else:
     print("Incorrect guess the number")
 
-try_again=True/False 
-print("1.Yes")
-print("2.No")
+num=0
+while(play2!=play1): #this conditon is not equal to match the number or play1 and play 2
+    print("Sorry, You guess the second time incorect number")
+    num+=1 #store the count of incorrect number
 
-choice=input("Choose the yes/no")
+    count=0  #count the times or guess the number
 
-if choice==1:
-    True
-    print("Yes")
-elif choice==2:
-    False
-    print("No")
-else:
-    print("Invalid choice")    
+    play1=str(play1) #explicitly conversion of integer to string
+    play2=str(play2) #explicitly conversion of integer to string
+ 
+    i=0
+    correct=['x']*3
 
 
+    for i in range(0,3):
 
+     if(play2[i]==play1[i]):
+      print("You guess the correct number")
 
+      count+=1
+     
+      correct[i]=play2[i]
 
+     else:
+      continue
 
+    print(f"You did get the {count} digits correct")
 
+    play2= int(input("Enter your next choice of numbers: "))
+
+    if(play2[i]==play1[i]):
+        num+=1
+        print("You are a mastermind")
+        print("It took you only", num, "tries.")
